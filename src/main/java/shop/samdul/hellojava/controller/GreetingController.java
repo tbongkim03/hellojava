567228573c55b34447b90d60ae7fbaf9f24fd879
tbongkim03 <tbongkim03@gmial.com>
2024-10-16 16:50:53 +0900

    greeting

--- src/main/java/shop/samdul/hellojava/controller/GreetingController.java
+++ src/main/java/shop/samdul/hellojava/controller/GreetingController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    ArrayList<String> plist = new ArrayList();
+    ArrayList<People> plist = new ArrayList();
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="HI") String name, Model model) {
         People p = new People();
-        p.name = name;
-        p.upname = p.upper(p.name);
-        plist.add(p.upname);
-        p.num = plist.indexOf(p.upname);
+        p.setName(name);
+        p.upper(p.getName());
+        plist.add(p);
+        p.setNum(plist.indexOf(p) + 1);
         model.addAttribute("plist", plist);
         return "greeting";
     }
--- src/main/java/shop/samdul/hellojava/controller/People.java
+++ src/main/java/shop/samdul/hellojava/controller/People.java
@@ -1,20 +1,32 @@
 package shop.samdul.hellojava.controller;
 
 public class People {
-    int num;
-    String name;
-    String upname;
-    
-    public String upper(String name) {
+    private int num;
+    private String name;
+
+    public int getNum() {
+        return num;
+    }    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void upper(String name) {
         if (name == null || name.isEmpty()) {
-            return ""; // 이름이 null이거나 비어있을 경우 처리
+            return;
         }
 
         String firstChar = name.substring(0, 1);
         String etcChar = name.substring(1);
 
         firstChar = firstChar.toUpperCase();
-        upname = firstChar + etcChar;
-        return upname;
+        setName(firstChar + etcChar);
     }
 }
