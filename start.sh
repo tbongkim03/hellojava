8054547e9941e90deca6c3007ff1d1068de905aa
tbongkim03 <tbongkim03@gmial.com>
2024-10-22 10:40:45 +0900

    start & stop

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the application
-nohup java \
-	-jar build/libs/hellojava-0.0.1-SNAPSHOT.jar &
+nohup java -jar ./app.jar > server.log 2>&1 &
 
-echo "> Application started with PID $!"
+sleep 10
+echo "> 실행 완료. 백그라운드에서 애플리케이션이 실행 중입니다."
--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-JAR_FILE_NAME=hellojava-0.0.1-SNAPSHOT.jar
+JAR_FILE_NAME=app.jar
 
 # Get the PID from the file
 PID=$(pgrep -f $JAR_FILE_NAME)
 
 if [ -z "$PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -9 $PID"
     kill -9 $PID
