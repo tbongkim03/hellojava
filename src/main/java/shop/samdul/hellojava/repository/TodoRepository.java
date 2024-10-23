22ae99db9c25f7a42555c2347562d6850bbd2572
tbongkim03 <tbongkim03@gmial.com>
2024-10-23 16:46:00 +0900

    jpa repository

---
+++ src/main/java/shop/samdul/hellojava/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package shop.samdul.hellojava.repository;
+
+import shop.samdul.hellojava.entity.TodoEntity;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+
+    
+}
